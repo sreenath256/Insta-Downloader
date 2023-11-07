ad0ea7073537539c40cd30f96944246c808d0734
Sreenath <sreenathsrp256@gmail.com>
2023-11-07 15:56:18 +0530

    Second

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,14 @@ function App() {
     }
   };
 
+    const handleButtonClick = () => {
+      // Open the video page in a new tab
+      window.open(videoData.media, '_blank');
+      // or
+      // Redirect to the video page
+      // window.location.href = 'https://example.com/video-page';
+    };
+
   return (
     <div className="container">
     
@@ -38,10 +46,11 @@ function App() {
           videoData && (
             <div className="video-section">
             
-               <video className="video-player" controls>
+               {/* <video className="video-player" controls>
                 <source src="https://instagram.fdnk3-1.fna.fbcdn.net/v/t66.30100-16/10000000_1061247398337217_7986528291713675059_n.mp4?_nc_ht=instagram.fdnk3-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=03gXe-ztFWQAX_yqiqV&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfAJsM3-AbVrex4EaMGZD1YBrTCJebbp2vIMnEaRf92Hqw&oe=654B8FD1&_nc_sid=2999b8" type="video/mp4" />
                 
-              </video> 
+              </video>  */}
+              <button className='button' onClick={handleButtonClick}>Click here</button>
               
             </div>
           )
